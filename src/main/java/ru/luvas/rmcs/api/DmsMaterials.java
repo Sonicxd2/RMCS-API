ef49f140e339f94ecae5637aba99dddf0742bc19
RinesThaix <iam@kostya.sexy>
2017-02-19 18:14:37 +0300

    v1.0.2.0: Доступ к нестандартным Material

---
+++ src/main/java/ru/luvas/rmcs/api/DmsMaterials.java
@@ -0,0 +1,30 @@
+package ru.luvas.rmcs.api;
+
+import org.bukkit.Material;
+
+/**
+ * Класс для доступа к материалам, которые нельзя получить прямым доступом к Material
+ * (ко всем тем, что были добавлены нами).
+ * Created by RINES on 19.02.17.
+ */
+public class DmsMaterials {
+
+    //Блоки
+    public static Material
+        PURPUR_BLOCK,
+        PURPUR_PILLAR,
+        END_BRICK,
+        END_GATEWAY,
+        MAGMA,
+        NETHER_WART_BLOCK,
+        RED_NETHER_BRICK,
+        BONE_BLOCK;
+
+    //Предметы
+    public static Material
+        TOTEM_OF_UNDYING,
+        BONE_SWORD,
+        BLOOD_SWORD,
+        DARK_SWORD;
+
+}
