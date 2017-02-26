6fe4aeb63ffd90ec7e963adc3fbd6cdeb4f2271e
RinesThaix <iam@kostya.sexy>
2017-02-26 03:17:10 +0300

    v1.0.2.4: Больше кастомных материалов

--- src/main/java/ru/luvas/rmcs/api/CommandsAPI.java
+++ src/main/java/ru/luvas/rmcs/api/CommandsAPI.java
@@ -12,13 +12,13 @@ public interface CommandsAPI {
      * Регистрация новой команды.
      * @param name название команды (без слеша).
      * @param usage подсказка к использованию команды.
-     * @param usagePredicte предикат использования: консоль никогда не может использовать команды, зарегистрированные
+     * @param usagePredicate предикат использования: консоль никогда не может использовать команды, зарегистрированные
      *                      данным образом, однако если команду использует игрок, то он будет прогоняться через этот
      *                      предикат, и команда будет допускаться к исполнению только в случае, если предикат вернет true.
      * @param command реализация команды.
      * @param aliases алиасы команды.
      */
-    void register(String name, String usage, PlayerPredicate usagePredicte, CommandInterface command, String... aliases);
+    void register(String name, String usage, PlayerPredicate usagePredicate, CommandInterface command, String... aliases);
 
     interface CommandInterface {
 
